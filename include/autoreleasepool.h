d8f0c017b9ff9f8b7ff9ee4926ce6014317b6b7d
zxsvrx <122689944+zxsvrx@users.noreply.github.com>
2023-06-20 19:14:44 +0200

    Update autoreleasepool.h

--- include/autoreleasepool.h
+++ include/autoreleasepool.h
@@ -28,7 +28,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define autoreleasepool(x) MMCreateReleasePool(); x MMReleaseCurrentPool();
+#define autoreleasepool(x) CreateAutoreleasepool(); x ReleaseCurrentPool();
 
 #define alloc(x) MMAlloc(x)
 
