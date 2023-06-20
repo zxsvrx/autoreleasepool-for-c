0d417fb2bc99f5e27b8f9ce6144687ffbf634b6a
zxsvrx <122689944+zxsvrx@users.noreply.github.com>
2023-06-20 19:25:41 +0200

    Create allocs.h

---
+++ include/allocs.h
@@ -0,0 +1,14 @@
+#ifndef allocs_h
+#define allocs_h
+
+#include "autoreleasepool.h"
+
+#define alloc(type, wantSize) (type*)alloc(sizeof(type)*wantSize)
+
+#define charAlloc   (char*)alloc
+#define intAlloc    (int*)alloc
+#define longAlloc   (long*)alloc
+#define floatAlloc  (float*)alloc
+#define doubleAlloc (double*)alloc
+
+#endif
