b5f7783a8a8c4770884ac645d757241cfc6ef2f3
zxsvrx <122689944+zxsvrx@users.noreply.github.com>
2023-06-21 14:30:53 +0200

    Update allocs.h

--- include/allocs.h
+++ include/allocs.h
@@ -4,11 +4,19 @@
 #include "autoreleasepool.h"
 
 #define alloc_b(type, n) (type*)alloc(sizeof(type)*n)
+#define preserve(type, n) (type*)AutoreleasePreserveAlloc(type, n)
+#define realloc_b(type, pool, n) (type*)AutpreleaseRealloc(pool, type, n)
 
 #define charAlloc(n)    (char*)alloc(sizeof(char)*n)
 #define intAlloc(n)     (int*)alloc(sizeof(int)*n)
 #define longAlloc(n)    (long*)alloc(sizeof(long)*n)
 #define floatAlloc(n)   (float*)alloc(sizeof(float)*n)
 #define doubleAlloc(n)  (double*)alloc(sizeof(double)*n)
+
+#define charRealloc(pool, ptr, n)   (char*)AutoreleaseRealloc(pool, ptr, sizeof(char)*n)
+#define intRealloc(pool, ptr, n)    (int*)AutoreleaseRealloc(pool, ptr, sizeof(int)*n)
+#define longRealloc(pool, ptr, n)   (long*)AutoreleaseRealloc(pool, ptr, sizeof(long)*n)
+#define floatRealloc(pool, ptr, n)  (float*)AutoreleaseRealloc(pool, ptr, sizeof(float)*n)
+#define doublRealloc(pool, ptr, n)  (double*)AutoreleaseRealloc(pool, ptr, sizeof(double)*n)
 
 #endif
